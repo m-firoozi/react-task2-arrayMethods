c8fff9d3a0edf7e9394fe69cd6c842b5ce5c2c26
m-firoozi <firoozimehrnoosh@yahoo.com>
2024-10-02 23:58:02 +0330

    second- commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 
 function App() {
   var numbers = [2, 46, 1, 38, 4];
+
+  //filter
   const oddNumbers=(odd)=>{
     return(
       odd % 2 !== 0
     )
   }
+
+  //reduce
+  const addition=(total,num) =>{
+    return(
+      total + num
+    )
+  }
+
+
   return (
     <>
     {numbers.map((number) => <p>{number}</p>)} 
@@ -13,6 +24,10 @@ function App() {
       <hr/> 
 
     {numbers.filter(oddNumbers)}
+
+    <hr/>
+
+    {numbers.reduce(addition)}
 
     <hr/>
 
